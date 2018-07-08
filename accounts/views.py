1afd2101b6e690f740e1072169b5c4474745b766
prateek tater <prateektater@gmail.com>
2018-07-08 02:48:19 -0400

    unique username and admin login required

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 
 # Create your views here.
@@ -7,8 +9,13 @@ from django.contrib.auth.models import User
 def signup(request):
 	if request.method == 'POST':
 		if request.POST['password1'] == request.POST['password2']:
-			User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-			return render(request, 'accounts/signup.html')
+			try:
+				user = User.objects.get(username = request.POST['username'])
+				return render(request, 'accounts/signup.html', { 'error': 'same username'})
+			except User.DoesNotExsist:
+				User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+				login(request, user)
+				return render(request, 'accounts/signup.html')
 		else:
 			return render(request, 'accounts/signup.html', { 'error': 'passwords did not match'})
 
