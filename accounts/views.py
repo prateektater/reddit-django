2b54b13704353c9457abd86d37e5a1d4db5139dd
prateek tater <prateektater@gmail.com>
2018-07-08 02:10:26 -0400

    adding users and passwords matching

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 def signup(request):
 	if request.method == 'POST':
-		print("the post worked")
+		if request.POST['password1'] == request.POST['password2']:
+			User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+			return render(request, 'accounts/signup.html')
+		else:
+			return render(request, 'accounts/signup.html', { 'error': 'passwords did not match'})
+
 
 	else:
 		return render(request, 'accounts/signup.html')
