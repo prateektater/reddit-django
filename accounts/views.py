2b3de2f93819359bab64dcc5d5582c198aaa90fc
prateek tater <prateektater@gmail.com>
2018-07-08 01:08:02 -0400

    sign up html page

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def signup(request):
-	return render(request, 'accounts/signup.html')
+	if request.method == 'POST':
+		print("the post worked")
+
+	else:
+		return render(request, 'accounts/signup.html')
+		
--- redditclone/urls.py
+++ redditclone/urls.py
@@ -19,6 +19,6 @@ import accounts.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^signup/', accounts.views.signup),
+    url(r'^signup/', accounts.views.signup, name = 'signup'),
 
 ]
